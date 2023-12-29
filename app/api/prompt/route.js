486745db4ce035cf0ac7cc8701be83e788a6d31c
hassanadawieh <adawiehhassan@gmail.com>
2023-12-29 15:22:34 +0200

    edit the caching in the prompt route directory

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,12 +2,17 @@ import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 export const GET = async (req) => {
-    try {
-        await connectToDB();
-        const prompts = await Prompt.find({}).populate('creator')
-        return new Response(JSON.stringify(prompts), 
-        {status: 200})
-    } catch (error) {
-        return new Response("Failed to fetch all prompts", {status : 500})
-    }
-}
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({}).populate("creator");
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store", // or other cache control directives
+      },
+    });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
